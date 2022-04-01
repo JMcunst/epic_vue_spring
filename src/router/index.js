5f34f17ba4238eb7581c2298e889fe87f5f01919
Jmcunst <xnslqjtmghaf@gmail.com>
2022-04-01 13:47:49 +0900

    update : route info

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Main from '../views/Main.vue';
-import Info from '../views/Info.vue';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: () => import('../views/About.vue'),
+    },
+]
+
 const router = new VueRouter({
 	mode:'history', //해쉬값 제거 방식
-    routes: [{
-        path: '/', 
-        redirect: '/home' 
-    }, {
-        path: '/home',
-        component: Main
-    }, {
-        path: '/Info',
-        component: Info
-    }]
+    base: process.env.BASE_URL,
+    routes,
 });
+
+export default router
